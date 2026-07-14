fb834770b8d24eb8d049e7fdc9e4203aa0bbd002
jefersonmarcolin-ops <jefersonmarcolin@gmail.com>
2026-07-14 17:53:33 -0300

    6 tentativa

--- scripts/obterdatas.js
+++
@@ -1,5 +0,0 @@
-const anoAtual = document.getElementById("anoatual");
-const ultimaModificacao = document.getElementById("ultimaModificacao");
-
-anoAtual.textContent = new Date().getFullYear();
-ultimaModificacao.textContent = `Última Modificação: ${document.lastModified}`;
